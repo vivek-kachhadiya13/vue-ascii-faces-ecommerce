2fc9558832876e2a4ff08852e70645a405579827
vivek <vivek.kachhadiya23@gmail.com>
2022-06-21 18:05:43 +0530

    feat: display random ads on welcome page on each page load

--- src/client/components/AdCard.vue
+++ src/client/components/AdCard.vue
@@ -1,13 +1,36 @@
-<script setup>
+<script>
 import { StarIcon } from '@heroicons/vue/outline';
+
+import { ref } from 'vue';
+
+import { API_URL, ADS_URL } from '@constants/index';
+
+export default {
+  components: {
+    StarIcon,
+  },
+  setup() {
+    // State
+    const addURL = ref(
+      `${API_URL}${ADS_URL}${parseInt(Date.now() * Math.random())}`
+    );
+
+    return {
+      addURL,
+    };
+  },
+};
 </script>
 
 <template>
   <div class="flex-col drop-shadow-lg rounded-lg lg:w-[440px] w-auto relative">
-    <img
-      src="https://picsum.photos/600/400/?random"
-      class="rounded-lg w-full h-[220px]"
-      loading="lazy" />
+    <div class="bg-slate-100 lg:w-[440px] w-auto">
+      <img
+        :src="addURL"
+        loading="lazy"
+        class="rounded-lg w-full h-56"
+        alt="picsum photo" />
+    </div>
     <div
       class="absolute bottom-0 flex items-center justify-between bg-[#00000099] w-full text-white rounded-b-lg p-3">
       <div>
@@ -16,10 +39,12 @@ import { StarIcon } from '@heroicons/vue/outline';
           Please take a word from our sponsors
         </p>
       </div>
-      <div
+      <a
+        href="https://unsplash.it"
+        target="_blank"
         class="bg-[#ff4081] rounded-full p-1 h-14 w-14 absolute flex justify-center items-center top-[-30px] right-3 cursor-pointer">
         <StarIcon class="h-8 w-8" />
-      </div>
+      </a>
     </div>
   </div>
 </template>
